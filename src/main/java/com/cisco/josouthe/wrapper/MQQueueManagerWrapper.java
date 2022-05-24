ec259f57ad58653bfd9202049cf4639ad60c7351
jbsouthe <jbsouthe@github.com>
2022-05-24 13:00:07 -0600

    figured out the correct way to construct the MQQueueManager based on some better examples

--- src/main/java/com/cisco/josouthe/wrapper/MQQueueManagerWrapper.java
+++ src/main/java/com/cisco/josouthe/wrapper/MQQueueManagerWrapper.java
@@ -2,9 +2,6 @@ package com.cisco.josouthe.wrapper;
 
 import com.appdynamics.instrumentation.sdk.template.AGenericInterceptor;
 import com.appdynamics.instrumentation.sdk.toolbox.reflection.IReflector;
-import com.appdynamics.instrumentation.sdk.toolbox.reflection.ReflectorException;
-
-import java.lang.reflect.Constructor;
 import java.util.Hashtable;
 
 public class MQQueueManagerWrapper extends BaseWrapper{
@@ -14,36 +11,21 @@ public class MQQueueManagerWrapper extends BaseWrapper{
     public MQQueueManagerWrapper(AGenericInterceptor aGenericInterceptor, JmsConnectionFactoryWrapper jmsConnectionFactoryWrapper) {
         super(aGenericInterceptor, null, null);
 
-        /*
-        IReflector constructor = interceptor.getNewReflectionBuilder()
-                .loadClass("com.ibm.mq.MQQueueManager")
-                .createObject("com.ibm.mq.MQQueueManager", String.class.getCanonicalName(), Hashtable.class.getCanonicalName())
-                .build();
-
-        Hashtable environment = jmsConnectionFactoryWrapper.getPropertyHashTable();
-        interceptor.getLogger().info(String.format("Created env hashtable with %d properties", environment.size()));
-        try { //TODO FIX THIS "WARN JavaAgent - Exception caught: java.lang.NoClassDefFoundError: com/ibm/mq/MQEnvironment"
-            this.object = constructor.execute( jmsConnectionFactoryWrapper.object.getClass().getClassLoader()
-                    , null
-                    , new Object[]{
-                        jmsConnectionFactoryWrapper.getStringProperty("XMSC_WMQ_QUEUE_MANAGER")
-                        ,environment
-                    }
-            );
-            interceptor.getLogger().info(String.format("Made it"));
-        } catch (ReflectorException e) {
-            interceptor.getLogger().info("ERROR Creating MQQueueManager, exception: "+ e.toString(), e);
-        } catch (RuntimeException runtimeException ) {
-            interceptor.getLogger().info("Runtime Exception Creating MQQueueManager, exception: "+ runtimeException.toString(), runtimeException);
-        }
-
-         */
 
         try{
-            Class<?> clazz = jmsConnectionFactoryWrapper.getObject().getClass().getClassLoader().loadClass("com.ibm.mq.MQQueueManager");
-            Constructor constructor = clazz.getConstructor( String.class, Hashtable.class);
-            Hashtable env = jmsConnectionFactoryWrapper.getPropertyHashTable("XMSC_WMQ_HOST_NAME", "XMSC_WMQ_PORT", "XMSC_WMQ_CHANNEL", "XMSC_USERID", "XMSC_PASSWORD");
-            this.object = constructor.newInstance(jmsConnectionFactoryWrapper.getStringProperty("XMSC_WMQ_QUEUE_MANAGER"), env);
+            Hashtable connectionProperties = new Hashtable<String,Object>(); //jmsConnectionFactoryWrapper.getPropertyHashTable("XMSC_WMQ_HOST_NAME", "XMSC_WMQ_PORT", "XMSC_WMQ_CHANNEL", "XMSC_USERID", "XMSC_PASSWORD");
+            connectionProperties.put("transport", "MQSeries Client");
+            connectionProperties.put("hostname", jmsConnectionFactoryWrapper.getObjectProperty("XMSC_WMQ_HOST_NAME"));
+            connectionProperties.put("port", jmsConnectionFactoryWrapper.getIntProperty("XMSC_WMQ_PORT"));
+            connectionProperties.put("channel", jmsConnectionFactoryWrapper.getObjectProperty("XMSC_WMQ_CHANNEL"));
+            connectionProperties.put("userID", jmsConnectionFactoryWrapper.getObjectProperty("XMSC_USERID"));
+            connectionProperties.put("password", jmsConnectionFactoryWrapper.getObjectProperty("XMSC_PASSWORD"));
+            logger.info(String.format("jms queue '%s' connection properties: %s", jmsConnectionFactoryWrapper.getStringProperty("XMSC_WMQ_QUEUE_MANAGER"), connectionProperties));
+            IReflector constructor = interceptor.getNewReflectionBuilder()
+                    .createObject("com.ibm.mq.MQQueueManager", String.class.getCanonicalName(), Hashtable.class.getCanonicalName() )
+                    .build();
+            this.object = constructor.execute( jmsConnectionFactoryWrapper.getObject().getClass().getClassLoader(), null,
+                    new Object[] { jmsConnectionFactoryWrapper.getStringProperty("XMSC_WMQ_QUEUE_MANAGER"), connectionProperties });
             logger.info("Initialized IBM MQ Queue Manager for monitoring, with reflection");
         } catch (Exception exception) {
             logger.info(String.format("Error initializing reflective queue manager for %s Exception: %s",jmsConnectionFactoryWrapper.getStringProperty("XMSC_WMQ_QUEUE_MANAGER"), exception.toString()),exception);
