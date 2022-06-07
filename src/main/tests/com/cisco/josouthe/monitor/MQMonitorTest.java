ae31e3701e9f4383792cae90398b19ba171d0389
jbsouthe <jbsouthe@github.com>
2022-06-07 13:17:37 -0600

    added unit test for a little quick help on getting queue name

---
+++ src/main/tests/com/cisco/josouthe/monitor/MQMonitorTest.java
@@ -0,0 +1,22 @@
+package com.cisco.josouthe.monitor;
+
+import junit.framework.TestCase;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class MQMonitorTest extends TestCase {
+
+    public void testGetQueueName() {
+        String queueName = "queue:///DEV.QUEUE.1";
+        String finalName = queueName;
+        try {
+            URI url = new URI(queueName);
+            finalName = url.getPath();
+        } catch (Exception exception) {
+            System.out.println(String.format("Error attempting to get path from '%s' Exception: %s", queueName, exception.toString()));
+        }
+        finalName = (finalName.startsWith("/") ? finalName.substring(1) : finalName);
+        System.out.println(String.format("in '%s' -> '%s' out",queueName, finalName));
+    }
+}
