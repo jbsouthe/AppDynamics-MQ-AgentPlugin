6c5d3d821da34eb45e042590feb21efc06587e60
jbsouthe <jbsouthe@github.com>
2022-06-08 14:10:49 -0600

    clean up of error handling

--- src/main/java/com/cisco/josouthe/exception/UserNotAuthorizedException.java
+++ src/main/java/com/cisco/josouthe/exception/UserNotAuthorizedException.java
@@ -1,18 +1,30 @@
 package com.cisco.josouthe.exception;
 
+import com.cisco.josouthe.wrapper.MQQueueManagerWrapper;
+
 public class UserNotAuthorizedException extends Throwable {
-    public String hostnamne, userID, channel;
+    public String hostnamne, userID, queueManagerName, channel;
     public Integer port;
 
-    public UserNotAuthorizedException( String hostname, Integer port, String userID, String channel, String msg ) {
+    public UserNotAuthorizedException( String hostname, Integer port, String userID, String queueManagerName, String channel, String msg ) {
         super(msg);
         this.hostnamne = hostname;
         this.port = port;
         this.userID = userID;
+        this.queueManagerName = queueManagerName;
         this.channel = channel;
     }
 
+    public UserNotAuthorizedException(MQQueueManagerWrapper mqQueueManager, String msg) {
+        super(msg);
+        this.hostnamne = mqQueueManager.getHostname();
+        this.port = mqQueueManager.getPort();
+        this.userID = mqQueueManager.getUserID();
+        this.queueManagerName = mqQueueManager.getName();
+        this.channel = mqQueueManager.getChannel();
+    }
+
     public String toString() {
-        return String.format("the user is not authorized: %s@%s:%p/channel=%s exception: %s", userID, hostnamne, port, channel, getMessage());
+        return String.format("the user is not authorized: %s@%s:%p/%s?channel=%s exception: %s", userID, hostnamne, port, queueManagerName, channel, getMessage());
     }
 }
--- src/main/java/com/cisco/josouthe/util/ExceptionUtility.java
+++ src/main/java/com/cisco/josouthe/util/ExceptionUtility.java
@@ -10,4 +10,18 @@ public class ExceptionUtility {
         }
         return exception;
     }
+
+    public static boolean exceptionMatches( Exception exception, String matchString ) {
+        if( exception == null || matchString == null ) return false;
+        if( exception.getCause() instanceof java.lang.reflect.InvocationTargetException ) {
+            Throwable rootException = exception.getCause();
+            if( rootException != null ) {
+                rootException = rootException.getCause();
+                if( rootException != null && "com.ibm.mq.headers.MQExceptionWrapper".equals(rootException.getClass().getCanonicalName()) ) {
+                    return matchString.equals(rootException.getMessage());
+                }
+            }
+        }
+        return false;
+    }
 }
