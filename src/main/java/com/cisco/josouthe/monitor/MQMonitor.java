bfe42da0d49637b52779d9f10d72e223c19ac36f
jbsouthe <jbsouthe@github.com>
2022-06-17 09:53:34 -0600

    completed q mgr and queue data collections

--- src/main/java/com/cisco/josouthe/monitor/MQMonitor.java
+++ src/main/java/com/cisco/josouthe/monitor/MQMonitor.java
@@ -9,6 +9,7 @@ import com.cisco.josouthe.util.MQConstants;
 import com.cisco.josouthe.wrapper.*;
 
 import java.net.URI;
+import java.text.ParseException;
 import java.util.*;
 
 public class MQMonitor extends BaseJMSMonitor {
@@ -26,7 +27,7 @@ public class MQMonitor extends BaseJMSMonitor {
         try {
             agent = new PCFMessageAgentWrapper(aGenericInterceptor, mqQueueManager );
         } catch (UserNotAuthorizedException e) {
-            logger.debug(String.format("Error creating PCFMessageAgent, %s", e.toString()));
+            logger.info(String.format("Error creating PCFMessageAgent, %s", e.toString()));
         }
 
         metrics = new ArrayList<>();
@@ -40,46 +41,82 @@ public class MQMonitor extends BaseJMSMonitor {
     @Override
     public void run() {
         logger.debug(String.format("IBM MQ Monitor run method called for %s", connectionFactoryWrapper.toString()));
+        reportQueueManagerStatus();
+
+        if( queues.isEmpty() ) queues.add("queue:///DEV.QUEUE.1"); //for testing
+        logger.debug(String.format("Queue names: %s", queues.toString()));
+        for( String qName : queues ) {
+            String shortQueueName = getQueueName(qName);
+            reportQueueStatus(shortQueueName);
+            reportQueueDepth(shortQueueName);
+        }
+    }
+
+    private void reportQueueDepth(String shortQueueName) {
+        try {
+            MQQueueWrapper mqQueueWrapper = mqQueueManager.accessQueue(shortQueueName, 8226);
+            logger.debug(String.format("MQQueue destQueue = qMgr.accessQueue(qName, openOptions);"));
+            int depthCurrent = mqQueueWrapper.getCurrentDepth();
+            logger.debug(String.format("int %d = destQueue.getCurrentDepth();", depthCurrent));
+            int depthMax = mqQueueWrapper.getMaximumDepth();
+            logger.debug(String.format("int %d = destQueue.getMaximumDepth();", depthMax));
+            mqQueueWrapper.close();
+
+            reportMetric(String.format("%s|%s|%s|Current Depth", this.providerName, mqQueueManager.getName(), shortQueueName), depthCurrent, "OBSERVATION", "AVERAGE", "COLLECTIVE");
+            reportMetric(String.format("%s|%s|%s|Maximum Depth", this.providerName, mqQueueManager.getName(), shortQueueName), depthMax, "OBSERVATION", "AVERAGE", "COLLECTIVE");
+        } catch (Exception mqException) {
+            Throwable sourceException = ExceptionUtility.getRootCause(mqException);
+            logger.debug(String.format("Error getting queue statistics from %s Exception: %s", shortQueueName, sourceException.toString()),sourceException);
+        }
+    }
+
+    private void reportQueueStatus(String shortQueueName) {
+        PCFMessageWrapper request = new PCFMessageWrapper(this.interceptor, agent.getObject(), MQConstants.getIntFromConstant("CMQCFC.MQCMD_INQUIRE_Q_STATUS"));
+        request.addParameter( MQConstants.getIntFromConstant("CMQC.MQCA_Q_NAME"), shortQueueName);
+        request.addParameter( MQConstants.getIntFromConstant("CMQCFC.MQIACF_Q_STATUS_ATTRS"), new Integer[]{ MQConstants.getIntFromConstant("CMQCFC.MQIACF_ALL") });
+        List<PCFMessageWrapper> responses = agent.send(request);
+        for( PCFMessageWrapper response : responses ) {
+            logger.trace(String.format("Response %s", response.toString()));
+            int curQFileSize = response.getIntParameterValue("MQIACF_CUR_Q_FILE_SIZE");
+            reportMetric(String.format("%s|%s|%s|Current Queue File Size", this.providerName, mqQueueManager.getName(), shortQueueName), curQFileSize, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            int maxQFileSize = response.getIntParameterValue("MQIACF_CUR_MAX_FILE_SIZE");
+            reportMetric(String.format("%s|%s|%s|Max Queue File Size", this.providerName, mqQueueManager.getName(), shortQueueName), maxQFileSize, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            reportMetric(String.format("%s|%s|%s|Percent Queue File Full", this.providerName, mqQueueManager.getName(), shortQueueName), (int) (curQFileSize/maxQFileSize), "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            int curInputCount = response.getIntParameterValue("MQIA_OPEN_INPUT_COUNT");
+            reportMetric(String.format("%s|%s|%s|Open Input Count", this.providerName, mqQueueManager.getName(), shortQueueName), curInputCount, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            int curOutputCount = response.getIntParameterValue("MQIA_OPEN_OUTPUT_COUNT");
+            reportMetric(String.format("%s|%s|%s|Open Output Count", this.providerName, mqQueueManager.getName(), shortQueueName), curOutputCount, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            try {
+                long lastGetTimestamp = getTimeStamp(response.getStringParameterValue("MQCACF_LAST_GET_DATE"), response.getStringParameterValue("MQCACF_LAST_GET_TIME"));
+                reportMetric(String.format("%s|%s|%s|Last Get Time (ms ago)", this.providerName, mqQueueManager.getName(), shortQueueName), getRelativeTime(lastGetTimestamp), "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            } catch (ParseException e) {
+                logger.info(String.format("Warning, timestamp could not be parsed for Last Get TimeStamp, error: %s",e.toString()));
+            }
+            try {
+                long lastPutTimestamp = getTimeStamp(response.getStringParameterValue("MQCACF_LAST_PUT_DATE"), response.getStringParameterValue("MQCACF_LAST_PUT_TIME"));
+                reportMetric(String.format("%s|%s|%s|Last Put Time (ms ago)", this.providerName, mqQueueManager.getName(), shortQueueName), getRelativeTime(lastPutTimestamp), "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            } catch (ParseException e) {
+                logger.info(String.format("Warning, timestamp could not be parsed for Last Put TimeStamp, error: %s",e.toString()));
+            }
+            int oldMsgAge = response.getIntParameterValue("MQIACF_OLDEST_MSG_AGE");
+            reportMetric(String.format("%s|%s|%s|Oldest Message Age", this.providerName, mqQueueManager.getName(), shortQueueName), oldMsgAge, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+            int uncommittedMsgs = response.getIntParameterValue("MQIACF_UNCOMMITTED_MSGS");
+            reportMetric(String.format("%s|%s|%s|Uncommitted Message Count", this.providerName, mqQueueManager.getName(), shortQueueName), uncommittedMsgs, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
+        }
+    }
+
+    private void reportQueueManagerStatus() {
         PCFMessageWrapper request = new PCFMessageWrapper(this.interceptor, agent.getObject(), MQConstants.getIntFromConstant("CMQCFC.MQCMD_INQUIRE_Q_MGR_STATUS"));
         request.addParameter(MQConstants.getIntFromConstant("CMQCFC.MQIACF_Q_MGR_STATUS_ATTRS"),
                 new Integer[] { MQConstants.getIntFromConstant("CMQCFC.MQIACF_ALL") });
         List<PCFMessageWrapper> responses = agent.send( request );
         for( PCFMessageWrapper response : responses ) {
             logger.trace(String.format("Response %s", response.toString()));
-            int status = response.getIntParameterValue( MQConstants.getIntFromConstant("MQIACF_Q_MGR_STATUS") );
+            int status = response.getIntParameterValue( "MQIACF_Q_MGR_STATUS" );
             logger.trace(String.format("MQIACF_Q_MGR_STATUS=%d",status));
             reportMetric(String.format("%s|%s|Status", this.providerName, mqQueueManager.getName()), status, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
-            int connectionCount = response.getIntParameterValue( MQConstants.getIntFromConstant("MQIACF_CONNECTION_COUNT"));
+            int connectionCount = response.getIntParameterValue("MQIACF_CONNECTION_COUNT");
             reportMetric(String.format("%s|%s|ConnectionCount", this.providerName, mqQueueManager.getName()), connectionCount, "OBSERVATION", "AVERAGE", "INDIVIDUAL");
-        }
-
-        if( queues.isEmpty() ) queues.add("queue:///DEV.QUEUE.1"); //for testing
-        logger.debug(String.format("Queue names: %s", queues.toString()));
-        for( String qName : queues ) {
-            String shortQueueName = getQueueName(qName);
-            request = new PCFMessageWrapper(this.interceptor, agent.getObject(), MQConstants.getIntFromConstant("CMQCFC.MQCMD_INQUIRE_Q_STATUS"));
-            request.addParameter( MQConstants.getIntFromConstant("CMQC.MQCA_Q_NAME"), shortQueueName);
-            request.addParameter( MQConstants.getIntFromConstant("CMQCFC.MQIACF_Q_STATUS_ATTRS"), new Integer[]{ MQConstants.getIntFromConstant("CMQCFC.MQIACF_ALL") });
-            responses = agent.send(request);
-            for( PCFMessageWrapper response : responses ) {
-                logger.debug(String.format("Response %s", response.toString()));
-            }
-
-            try {
-                MQQueueWrapper mqQueueWrapper = mqQueueManager.accessQueue(shortQueueName, 8226);
-                logger.debug(String.format("MQQueue destQueue = qMgr.accessQueue(qName, openOptions);"));
-                int depthCurrent = mqQueueWrapper.getCurrentDepth();
-                logger.debug(String.format("int %d = destQueue.getCurrentDepth();", depthCurrent));
-                int depthMax = mqQueueWrapper.getMaximumDepth();
-                logger.debug(String.format("int %d = destQueue.getMaximumDepth();", depthMax));
-                mqQueueWrapper.close();
-
-                reportMetric(String.format("%s|%s|%s|Current Depth", this.providerName, mqQueueManager.getName(), shortQueueName), depthCurrent, "OBSERVATION", "AVERAGE", "COLLECTIVE");
-                reportMetric(String.format("%s|%s|%s|Maximum Depth", this.providerName, mqQueueManager.getName(), shortQueueName), depthMax, "OBSERVATION", "AVERAGE", "COLLECTIVE");
-            } catch (Exception mqException) {
-                Throwable sourceException = ExceptionUtility.getRootCause(mqException);
-                logger.debug(String.format("Error getting queue statistics from %s Exception: %s",qName, sourceException.toString()),sourceException);
-            }
         }
     }
 
