68b0dd992aa3e94d612b22812923396ec6fda334
jbsouthe <jbsouthe@github.com>
2022-06-16 18:03:16 -0600

    removed dangerous setParameter(int, int) and added a new setParameter(int, String)

--- src/main/java/com/cisco/josouthe/wrapper/PCFMessageWrapper.java
+++ src/main/java/com/cisco/josouthe/wrapper/PCFMessageWrapper.java
@@ -7,7 +7,7 @@ import com.appdynamics.instrumentation.sdk.toolbox.reflection.IReflector;
 import java.util.Arrays;
 
 public class PCFMessageWrapper extends BaseWrapper{
-    private IReflector addParameterArray, addParameterAtomic, getIntParameterValue, constructor, getStringParameterValue, toString;
+    private IReflector addParameterIntArray, addParameterString, getIntParameterValue, constructor, getStringParameterValue, toString;
 
     public PCFMessageWrapper(ASDKPlugin aGenericInterceptor, Object parentObject, Integer creationOptions) {
         super(aGenericInterceptor, null, parentObject);
@@ -27,24 +27,23 @@ public class PCFMessageWrapper extends BaseWrapper{
 
     protected void initMethods() {
         constructor = interceptor.getNewReflectionBuilder().createObject("com.ibm.mq.headers.pcf.PCFMessage", new String[] { int.class.getCanonicalName() }).build();
-        addParameterArray = makeInvokeInstanceMethodReflector("addParameter", int.class.getCanonicalName(), "[I" );
-        addParameterAtomic = makeInvokeInstanceMethodReflector("addParameter", int.class.getCanonicalName(), int.class.getCanonicalName() );
+        addParameterIntArray = makeInvokeInstanceMethodReflector("addParameter", int.class.getCanonicalName(), "[I" );
+        addParameterString = makeInvokeInstanceMethodReflector("addParameter", int.class.getCanonicalName(), String.class.getCanonicalName() );
         getIntParameterValue = makeInvokeInstanceMethodReflector("getIntParameterValue", int.class.getCanonicalName());
         getStringParameterValue = makeInvokeInstanceMethodReflector( "getStringParameterValue", int.class.getCanonicalName());
         toString = makeInvokeInstanceMethodReflector("toString");
     }
 
     public void addParameter(int parameter, int[] values ) {
-        getReflectiveObject(this.object, addParameterArray, parameter, values);
+        getReflectiveObject(this.object, addParameterIntArray, parameter, values);
     }
 
     public void addParameter( int parameter, Integer[] integerValues ) {
         addParameter(parameter, Arrays.stream(integerValues).mapToInt(Integer::intValue).toArray());
     }
 
-    @Deprecated //this causes the queue manager to crash under certain conditions! always send an int array
-    public void addParameter( int parameter, int value ) {
-        getReflectiveObject(this.object, addParameterAtomic, parameter, value);
+    public void addParameter( int parameter, String value ) {
+        getReflectiveObject(this.object, addParameterString, parameter, value);
     }
 
     public Integer getIntParameterValue( int parameter ) {
