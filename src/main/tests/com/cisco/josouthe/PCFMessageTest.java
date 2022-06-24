e63deb6ee80d2f45b26e0d24127e107fc2254902
jbsouthe <jbsouthe@github.com>
2022-06-24 08:56:43 -0600

    moved local unit test mq auth details to a local property file to appease info sec scanners

--- src/main/tests/com/cisco/josouthe/PCFMessageTest.java
+++ src/main/tests/com/cisco/josouthe/PCFMessageTest.java
@@ -4,7 +4,6 @@ import com.appdynamics.agent.sdk.impl.NoOpLogger;
 import com.appdynamics.instrumentation.sdk.ASDKPlugin;
 import com.appdynamics.instrumentation.sdk.Rule;
 import com.appdynamics.instrumentation.sdk.logging.ISDKLogger;
-import com.cisco.josouthe.wrapper.MQQueueManagerWrapper;
 import com.ibm.mq.MQException;
 import com.ibm.mq.MQQueueManager;
 import com.ibm.mq.constants.CMQC;
@@ -12,14 +11,16 @@ import com.ibm.mq.constants.CMQCFC;
 import com.ibm.mq.headers.MQDataException;
 import com.ibm.mq.headers.pcf.PCFMessage;
 import com.ibm.mq.headers.pcf.PCFMessageAgent;
-import junit.framework.TestCase;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Properties;
 
 public class PCFMessageTest extends Assume {
     MQQueueManager queueManager = null;
@@ -27,14 +28,20 @@ public class PCFMessageTest extends Assume {
 
     @Before
     public void setUp() {
+        Properties testProps = new Properties();
+        try {
+            testProps.load(new FileInputStream(new File("TestAuth.properties")));
+        } catch ( IOException ioException ) {
+            System.out.println("Setup the ./TestAuth.properties file so the unit tests can run, message: "+ ioException.toString());
+        }
         try {
             Hashtable connectionProperties = new Hashtable<String, Object>(); //jmsConnectionFactoryWrapper.getPropertyHashTable("XMSC_WMQ_HOST_NAME", "XMSC_WMQ_PORT", "XMSC_WMQ_CHANNEL", "XMSC_USERID", "XMSC_PASSWORD");
-            connectionProperties.put("transport", "MQSeries Client");
-            connectionProperties.put("hostname", "localhost");
-            connectionProperties.put("port", 1414);
-            connectionProperties.put("channel", "MONITOR.SRVCONN");
-            connectionProperties.put("userID", "app");
-            connectionProperties.put("password", "passw0rd");
+            connectionProperties.put("transport", testProps.getProperty("mq.transport","MQSeries Client"));
+            connectionProperties.put("hostname", testProps.getProperty("mq.hostname","localhost"));
+            connectionProperties.put("port", Integer.valueOf(testProps.getProperty("mq.port", "1414") ) );
+            connectionProperties.put("channel", testProps.getProperty("mq.channel","MONITOR.SRVCONN"));
+            connectionProperties.put("userID", testProps.getProperty("mq.user"));
+            connectionProperties.put("password", testProps.getProperty("mq.pass"));
             queueManager = new MQQueueManager("QM1", connectionProperties);
             pcfMessageAgent = new PCFMessageAgent(queueManager);
             pcfMessageAgent.setWaitInterval(5);
@@ -88,28 +95,7 @@ public class PCFMessageTest extends Assume {
             System.out.println(String.format("response %s", response.toString()));
         }
     }
-
-
-
-    /*
-    @Test
-    public void testPCFMessageAgentSendReflection() throws Exception {
-        //not yet working
-
-        ASDKPlugin asdkPlugin = new ASDKPluginImpl();
-        Hashtable connectionProperties = new Hashtable<String,Object>(); //jmsConnectionFactoryWrapper.getPropertyHashTable("XMSC_WMQ_HOST_NAME", "XMSC_WMQ_PORT", "XMSC_WMQ_CHANNEL", "XMSC_USERID", "XMSC_PASSWORD");
-        connectionProperties.put("transport", "MQSeries Client");
-        connectionProperties.put("hostname", "localhost");
-        connectionProperties.put("port", 1414);
-        connectionProperties.put("channel", "MONITOR.SRVCONN");
-        connectionProperties.put("userID", "app");
-        connectionProperties.put("password", "passw0rd");
-        MQQueueManagerWrapper queueManager = new MQQueueManagerWrapper( asdkPlugin, "QM1", connectionProperties);
-
-
-    }
-
-     */
+    
 
     public class ASDKPluginImpl extends ASDKPlugin {
 
